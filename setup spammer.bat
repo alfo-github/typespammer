46bf902c78d7ccd7a82d04c9b64b0d903aff3766
alfo-discord <73135787+alfo-discord@users.noreply.github.com>
2020-11-11 10:00:51 +0100

    Add files via upload

---
+++ setup spammer.bat
@@ -0,0 +1,19 @@
+py -3 -m pip install pyautogui
+
+py -3 -m pip install keyboard
+
+py -3 -m pip install time 
+
+py -3 -m pip install colorama
+
+pauseGacha merda
+
+Gacha merda
+
+Gacha merda
+
+Gacha merda
+
+
+
+Gach
